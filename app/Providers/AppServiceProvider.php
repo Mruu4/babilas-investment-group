a89970e77cfa53efb65fabea81142d52c0b5a937
Umar-Faruq Salihu <you@example.com>
2026-09-10 02:26:35 +0300

    Force HTTPS scheme in production to fix mixed-content asset loading on Render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('components.layouts.admin', \App\View\Composers\AdminNavComposer::class);
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
